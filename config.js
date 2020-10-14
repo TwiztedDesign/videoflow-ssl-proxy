e7de99e5164c84e5ea9b83446a65b56b9769677f
Dok <ddumansky@gmail.com>
2020-10-14 14:36:29 +0300

    server config

---
+++ config.js
@@ -0,0 +1,73 @@
+const server = {
+	port: process.env.PORT || 80, // http port is needed for LetsEncrypt challenge during request / renewal. Also enables automatic http->https redirection for registered https routes.
+	letsencrypt: {
+		path: __dirname + '/certs',
+		port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
+	},
+	ssl: {
+		http2: true,
+		port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
+	},
+	xfwd: true
+};
+
+
+module.exports = {
+
+	development : {
+
+		server,
+
+		routes : [
+			{
+				from    : "ssl1.localhost.io",
+				to   : "http://127.0.0.1:3002",
+				options : {
+					ssl: {
+						letsencrypt: {
+							email: 'ddumansky@gmail.com', // Domain owner/admin email
+							production: false, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+						}
+					}
+				}
+			},
+			// {
+			// 	from    : "ssl2.localhost.io",
+			// 	to   : "http://github.com",
+			// 	options : {
+			// 		ssl: {
+			// 			letsencrypt: {
+			// 				email: 'ddumansky@gmail.com', // Domain owner/admin email
+			// 				production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+			// 			}
+			// 		}
+			// 	}
+			// }
+		]
+	},
+
+
+	production : {
+
+		server,
+
+		routes : [
+			{
+				from    : "sslproxy1.videoflow.io",
+				to   : "http://vfc-rjso5f-r8.videoflow.io",
+				options : {
+					ssl: {
+						letsencrypt: {
+							email: 'jan.umansky@twizted-design.com', // Domain owner/admin email
+							production: false, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+						}
+					}
+				}
+			},
+			{
+				from    : "sslproxy2.videoflow.io",
+				to   : "http://github.com"
+			}
+		]
+	}
+};
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const proxy = require('redbird')({port: process.env.PORT || 3003});
 
-proxy.register("sslproxy1.videoflow.io", "https://www.google.com");
-proxy.register("sslproxy2.videoflow.io", "https://github.com");
+const config  = require('./config')[process.env.NODE_ENV || 'development'];
+const proxy = require('redbird')(config.server);
+
+config.routes.forEach(route => {
+	proxy.register(route.from, route.to, route.options);
+})
+
+
+
