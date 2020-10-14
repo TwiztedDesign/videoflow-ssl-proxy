e60fc86599c666676a8d4a4c0e7e22158a0d9cf4
Dok <ddumansky@gmail.com>
2020-10-14 13:38:05 +0300

    server port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const proxy = require('redbird')({port: 80});
+const proxy = require('redbird')({port: process.env.PORT || 3003});
 
 proxy.register("sslproxy1.videoflow.io", "https://www.google.com");
 proxy.register("sslproxy2.videoflow.io", "https://github.com");
