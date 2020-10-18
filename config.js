82a55b7edd6385b7fd41b20b1bf2951630f74048
Dok <ddumansky@gmail.com>
2020-10-18 14:30:56 +0300

    server config

--- config.js
+++ config.js
@@ -76,6 +76,18 @@ module.exports = {
 					}
 				}
 			},
+			{
+				from : "defined-wasabi-l5yr3vepvwygt207njxj7yln.herokudns.com",
+				to   : "http://vfc-rjso5f-r8.videoflow.io",
+				options : {
+					ssl: {
+						letsencrypt: {
+							email: 'jan.umansky@twizted-design.com', // Domain owner/admin email
+							production: true, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+						}
+					}
+				}
+			},
 
 			// {
 			// 	from    : "sslproxy2.videoflow.io",
