e7da224c3f6c44b6fae0b07165109b24a1122af7
Dok <ddumansky@gmail.com>
2020-10-18 17:49:06 +0300

    remove custom resolver

--- config.js
+++ config.js
@@ -9,9 +9,9 @@ customResolver1.priority = 100;
 
 const server = {
 	port: process.env.PORT || 80, // http port is needed for LetsEncrypt challenge during request / renewal. Also enables automatic http->https redirection for registered https routes.
-	resolvers: [
-		customResolver1
-	],
+	// resolvers: [
+	// 	customResolver1
+	// ],
 	// letsencrypt: {
 	// 	path: __dirname + '/certs',
 	// 	port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
