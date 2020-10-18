c1e4d1a2de3addea3ea0f7bfb33bb1b98903d445
Dok <ddumansky@gmail.com>
2020-10-18 14:51:53 +0300

    server config

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const server = {
 	port: process.env.PORT || 80, // http port is needed for LetsEncrypt challenge during request / renewal. Also enables automatic http->https redirection for registered https routes.
-	letsencrypt: {
-		path: __dirname + '/certs',
-		port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
-	},
+	// letsencrypt: {
+	// 	path: __dirname + '/certs',
+	// 	port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
+	// },
 	// ssl: {
 	// 	http2: true,
 	// 	port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
