b7008c8aab02699a705902e769111eb5568dcd4c
Dok <ddumansky@gmail.com>
2020-10-18 15:06:42 +0300

    server config

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 const config  = require('./config')[process.env.NODE_ENV || 'development'];
 const proxy = require('redbird')(config.server);
 
+
 config.routes.forEach(route => {
 	proxy.register(route.from, route.to, route.options);
 })
+
+console.log('Server started on port:' , process.env.PORT || 80);
 
 
 
