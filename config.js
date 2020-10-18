38b5437422210c053624fe3812424b766071d357
Dok <ddumansky@gmail.com>
2020-10-18 14:49:46 +0300

    server config

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const server = {
 		path: __dirname + '/certs',
 		port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
 	},
-	ssl: {
-		http2: true,
-		port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
-	},
+	// ssl: {
+	// 	http2: true,
+	// 	port: 443, // SSL port used to serve registered https routes with LetsEncrypt certificate.
+	// },
 	xfwd: true,
 	bunyan : true
 };
