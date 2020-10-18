21ee3ab879df8d0e0956e22a95bf2df504744ae6
Dok <ddumansky@gmail.com>
2020-10-18 17:59:01 +0300

    enable ssl

--- config.js
+++ config.js
@@ -66,7 +66,7 @@ module.exports = {
 		routes : [
 			{
 				from : "sslproxy2.videoflow.io",
-				to   : "https://vfc-rjso5f-r8.videoflow.io",
+				to   : "https://www.videoflow.io/channel/vfc-rjso5f-r8",
 				options : {
 					ssl: {
 						letsencrypt: {
