7a30ae5874fd3a57a1b4aa612d8169ee49aacc91
Dok <ddumansky@gmail.com>
2020-10-18 15:03:59 +0300

    server config

--- config.js
+++ config.js
@@ -1,5 +1,17 @@
+const customResolver1 = function(host, url, req) {
+	console.log("HOST:", host);
+	console.log("URL:", url);
+};
+
+// assign high priority
+customResolver1.priority = 100;
+
+
 const server = {
 	port: process.env.PORT || 80, // http port is needed for LetsEncrypt challenge during request / renewal. Also enables automatic http->https redirection for registered https routes.
+	resolvers: [
+		customResolver1
+	],
 	// letsencrypt: {
 	// 	path: __dirname + '/certs',
 	// 	port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
@@ -24,12 +36,12 @@ module.exports = {
 				from    : "ssl1.localhost.io",
 				to   : "http://127.0.0.1:3002",
 				options : {
-					ssl: {
-						letsencrypt: {
-							email: 'ddumansky@gmail.com', // Domain owner/admin email
-							production: false, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
-						}
-					}
+					// ssl: {
+					// 	letsencrypt: {
+					// 		email: 'ddumansky@gmail.com', // Domain owner/admin email
+					// 		production: false, // WARNING: Only use this flag when the proxy is verified to work correctly to avoid being banned!
+					// 	}
+					// }
 				}
 			},
 			// {
